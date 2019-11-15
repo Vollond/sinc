23e2b0bbc26e9255c03418ad052e6ac41d95f07b
Vollond <34167687+Vollond@users.noreply.github.com>
2019-11-15 15:28:30 +0200

    1
    1

--- example/views.py
+++ example/views.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-
+import random
 from django.http import HttpResponse
 
-
+rand=random.randint(1,10)
 def index(request):
     now = datetime.now()
     html = f'''
     <html>
         <body>
             <h1>Hello from Zeit Now! SICN</h1>
+            <h1>Hello from Zeit Now! {rand}.</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
